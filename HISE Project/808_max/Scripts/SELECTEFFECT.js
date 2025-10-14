f43d4b2a1b9d7bf77ca123567f586284c5ee315f
zietraore <2610zitraore@student.epfc.eu>
2025-10-14 21:58:24 +0200

    Netoyage: qlq fichiers js, Multiband, components

--- HISE Project/808_max/Scripts/SELECTEFFECT.js
+++ HISE Project/808_max/Scripts/SELECTEFFECT.js
@@ -14,13 +14,13 @@ namespace SelectEffect
 	const pnlEffectFilter = getAny(["pnlEffectFilter", "pnlFilter"]);
 	const pnlEffectSaturation = getAny(["pnlEffectSaturation", "pnlSaturator"]);
 	const pnlEffectLFO = getAny(["pnlEffectLFO", "pnlLFO"]);
-	const pnlEffectMB = getAny(["pnlEffectMultiBandComp", "pnlMultiBandComp"]);
+
 
 	// View buttons (only one should be active at a time). Be tolerant of a stray space in the ID.
 	const btnEffectViewFilter = getAny(["btnEffectViewFilter "]);
 	const btnEffectViewSaturator = getAny(["btnEffectViewSaturator"]);
 	const btnEffectViewLFO = getAny(["btnEffectViewLFO"]);
-	const btnEffectViewMultiBand = getAny(["btnEffectViewMultiBand"]);
+
 
 	// ON / OFF buttons (independent from view selection)
 	// These are already wired via processorId / parameterId in the UI XML.
@@ -28,7 +28,6 @@ namespace SelectEffect
 	const btnONFilter = Content.getComponent("btnONFilter");
 	const btnONSaturator = Content.getComponent("btnONSaturator");
 	const btnONLfo = Content.getComponent("btnONLfo");
-	const btnONMultiBand = Content.getComponent("btnONMultiBand"); 
 
 	inline function setView(name)
 	{
@@ -37,7 +36,7 @@ namespace SelectEffect
 		if (pnlEffectLFO) pnlEffectLFO.set("visible", name == "LFO");		
 		if (pnlEffectMB) pnlEffectMB.set("visible", name == "MultiBand");
 
-		if (btnEffectViewMultiBand) btnEffectViewMultiBand.setValue(name=="MultiBand" ? 1: 0); 
+ 
 		if (btnEffectViewFilter) btnEffectViewFilter.setValue(name == "Filter" ? 1 : 0);
 		if (btnEffectViewSaturator) btnEffectViewSaturator.setValue(name == "Saturator" ? 1 : 0);
 		if (btnEffectViewLFO) btnEffectViewLFO.setValue(name == "LFO" ? 1 : 0);
@@ -59,22 +58,18 @@ namespace SelectEffect
 		if (value) setView("LFO");
 	}
 	
-	inline function onbtnEffectViewMultiBandControl(component, value)
-	{
-		if(value) setView("MultiBand");
-	}
+
 
 	// Hook up callbacks if the components exist
 	if (btnEffectViewFilter) btnEffectViewFilter.setControlCallback(onbtnEffectViewFilterControl);
 	if (btnEffectViewSaturator) btnEffectViewSaturator.setControlCallback(onbtnEffectViewSaturatorControl);
 	if (btnEffectViewLFO) btnEffectViewLFO.setControlCallback(onbtnEffectViewLFOControl);
-	if (btnEffectViewMultiBand) btnEffectViewMultiBand.setControlCallback(onbtnEffectViewMultiBandControl);
+
 
 	// Initial view: try to respect current visibility; otherwise default to Saturator
-	if (pnlEffectMB && pnlEffectMB.get("visible")) setView("MultiBand");
+	if (pnlEffectFilter && pnlEffectFilter.get("visible")) setView("Filter");
 	else if (pnlEffectSaturation && pnlEffectSaturation.get("visible")) setView("Saturator");
 	else if (pnlEffectLFO && pnlEffectLFO.get("visible")) setView("LFO");
-	else if(pnlEffectFilter && pnlEffectFilter.get("visible")) setView("Filter");
 	else setView("MultiBand");
 	
 }
